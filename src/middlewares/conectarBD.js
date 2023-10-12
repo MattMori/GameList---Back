3506d18ca45ee678513b0e21b9c3b6b2c5072a6b
MattMori <Mateus5310@live.com>
2023-10-12 06:20:46 -0300

    configuração do banco de dados

--- src/middlewares/conectarBD.js
+++ src/middlewares/conectarBD.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://game-list-back.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 async function conectarBancoDados(req = null, res = null, next = null) {
   try {
     await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
